e6dc47eedae01a1d3647ecf44d52b1025acfbb80
Valentijn Ev1l0rd <valentijnvermeij@gmail.com>
2017-11-15 18:04:46 +0100

    Add readme + license [ci-skip]

---
+++ README.md
@@ -0,0 +1,19 @@
+## xdicey
+
+[![NPM](https://nodei.co/npm/xdicey.png)](https://nodei.co/npm/xdicey/)
+
+Throw an x sided dice y amount of times.
+
+### Example
+```
+require("xdicey")(6,1) // Throws a six sided die one time
+```
+
+### Usage
+```
+require("xdicey")(y,x)
+```
+
+`y` is the amount of sides the die has. `x` is the amount of times the die should be thrown.
+
+The module will return the total sum of all dice thrown.
--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 /*
+*   xdicey - Throw an x sided dice y amount of times
+    Copyright (C) 2017 Valentijn "ev1l0rd" <contact@ev1l0rd.info>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+* /
+/*
  * Throws an x sided dice y amount of times
  * @param amountOfRolls Amount of times to throw the dice
  * @param amountOfSides Amount of sides the dice has
